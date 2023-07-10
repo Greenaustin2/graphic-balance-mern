19165030c6f1012f130bcc21bcae23f26a0f21b0
Greenaustin2 <greenaustin2@gmail.com>
2023-07-09 20:26:26 -0400

    axios url api

--- client/src/hooks/usePlayerArchive.js
+++ client/src/hooks/usePlayerArchive.js
@@ -32,7 +32,7 @@ function usePlayerArchive() {
   //sort direction accepts either 1 or -1, indicating ascending and descending values
   function loadVideoArchive(sortKey, sortDirection) {
     axios
-      .get("/archive", {
+      .get("http://localhost:5000/archive", {
         params: {
           sortKey: sortKey,
           sortDirection: sortDirection,
@@ -49,7 +49,7 @@ function usePlayerArchive() {
 
   const handleDelete = () => {
     axios
-      .delete("/archive/" + currentVideo)
+      .delete("http://localhost:5000/archive/" + currentVideo)
       .then(() => {
         console.log("video deleted");
       })
