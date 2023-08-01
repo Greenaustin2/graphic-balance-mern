bae00b9964ad72f08fb4aff20671d6244b18f76e
Greenaustin2 <greenaustin2@gmail.com>
2023-07-31 22:05:43 -0400

    axios adjustments

--- server/server.js
+++ server/server.js
@@ -13,8 +13,8 @@ const archiveRouter = require("./routes/archive");
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 
-  app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
   });
 }
 
