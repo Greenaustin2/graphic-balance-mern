4724ad644af96f09a149199ceab9c7f0bd1d0cef
Greenaustin2 <greenaustin2@gmail.com>
2023-08-02 17:05:26 -0400

    cors options updated

--- server/server.js
+++ server/server.js
@@ -8,7 +8,14 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://graphic-balance-mern.vercel.app",
+    credentials: true, // Allow sending cookies and authentication headers
+    methods: "GET, POST, PUT, DELETE", // Specify allowed HTTP methods
+    allowedHeaders: "Authorization, Content-Type", // Specify allowed headers
+  })
+);
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
@@ -21,6 +28,10 @@ connection.once("open", () => {
 const archiveRouter = require("./routes/archive");
 app.use("/archive-data", archiveRouter);
 
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
+
 // if (process.env.NODE_ENV === "production") {
 //   app.use(express.static(path.join(__dirname, "../client/build")));
 
@@ -28,7 +39,3 @@ app.use("/archive-data", archiveRouter);
 //     res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 //   });
 // }
-
-app.listen(port, () => {
-  console.log(`server is running on port ${port}`);
-});
