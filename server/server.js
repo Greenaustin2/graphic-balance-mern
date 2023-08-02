3831ff5231d880e3fc0b9f359d04b5d9110b352f
Greenaustin2 <greenaustin2@gmail.com>
2023-08-02 00:13:02 -0400

    axios baseURL

--- server/server.js
+++ server/server.js
@@ -2,33 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const path = require("path");
-
 require("dotenv").config();
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
 const port = process.env.PORT || 5000;
 
 const archiveRouter = require("./routes/archive");
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-
-  app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-  });
-}
-
-const corsOptions = {
-  origin: "https://graphic-balance-mern.vercel.app",
-  credentials: false,
-};
-
-app.use(cors(corsOptions));
-app.use(express.json());
-
-// let corsOptions = {
-//   origin: ["URL ALLOWED"],
-// };
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true });
@@ -36,7 +18,16 @@ const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MongoDB Atlas connection established");
 });
+
 app.use("/archive-data", archiveRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
